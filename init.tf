3e876ebce1fe777822fb1116c36c0484264a2282
uptown <uptownhr@gmail.com>
2022-04-18 05:13:06 -0700

    enabled terraform backend with s3

--- init.tf
+++ init.tf
@@ -1,6 +1,49 @@
+terraform {
+  backend "s3" {
+    bucket = "james-terraform-example-state"
+    key =  "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    profile = "james"
+
+    dynamodb_table = "james-terraform-example-locks"
+    encrypt = true
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
   profile = "james"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "james-terraform-example-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "james-terraform-example-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
 
 resource "aws_launch_configuration" "example" {
